f52421ee8ca9c25c68e4502769d20d04744a0a8e
NKV <vi.nguyen@sens-vn.com>
2023-02-07 14:40:34 +0700

    init

--- react/features/base/modal/components/styles.js
+++ react/features/base/modal/components/styles.js
@@ -1,8 +1,11 @@
+import { Dimensions } from 'react-native';
+const { height } = Dimensions.get('screen');
 
 export default {
 
     jitsiScreenContainer: {
-        flex: 1
+        flex: 1,
+        height: height / 2
     },
 
     safeArea: {
